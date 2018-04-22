4f4165672d0455c37d1a09db8035453c6494302d
Fawzi Benamor <f.benamor@root.sa>
2018-04-22 22:38:24 +0100

    Add project files.

---
+++ Calendar/Controllers/EventsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Calendar.Models;
+
+namespace Calendar.Controllers
+{
+    public class EventsController : Controller
+    {
+        private CalendarContext db = new CalendarContext();
+
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+         
+                var events = db.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            var status = false;
+         
+                if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = db.Events.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                //e.End = DateTime.Now.AddDays(1);
+                //e.Start = DateTime.Now;
+               // e.End = null;
+                db.Events.Add(e);
+                }
+
+                db.SaveChanges();
+                status = true;
+
+           
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+           
+                var v = db.Events.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    db.Events.Remove(v);
+                    db.SaveChanges();
+                    status = true;
+                }
+            
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
